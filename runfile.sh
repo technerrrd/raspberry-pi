17e90dff86e784e128af2145613f3fa54d674ca2
Anirudh Mathur <94562196+technerrrd@users.noreply.github.com>
2024-02-08 13:57:42 +0530

    updated docker-compose

--- runfile.sh
+++ runfile.sh
@@ -3,8 +3,8 @@ git config --global user.name "technerd"
 git config --global user.email "mathuranirudh1998@gmail.com"
 sudo apt-get update -y #packages update
 sudo apt-get upgrade -y #packages upgrade
-sudo apt dist-upgrade
-sudo apt clean
+sudo apt dist-upgrade -y
+sudo apt clean -y
 curl -sSL https://get.docker.com 67 | sh #install docker
 sudo usermod -aG docker sniperx07 #enable docker for sniperx07
 sudo systemctl enable docker
@@ -13,5 +13,5 @@ cp -r ~/raspberry-pi/docker ~/docker
 sudo docker run hello-world 
 cd ~/docker/combined
 sudo docker compose up -d
-sudo rpi-update #update firmware in the end
+sudo rpi-update -y #update firmware in the end
 sudo reboot#reboot
