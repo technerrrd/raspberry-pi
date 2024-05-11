2c8b3aed7fad1b9e8e48b593474cd17a38fa7fc0
technerrrd <94562196+technerrrd@users.noreply.github.com>
2024-05-11 12:06:09 +0530

    added environmental variables,removed nextcloud-wireguard-pihole folder,added external drive mount

--- runfile.sh
+++ runfile.sh
@@ -1,18 +1,20 @@
 echo "hello World"
-dir='/home/sniperx07'
+username='sniperx07'
+dir='/home/$username'
 git config --global user.name "technerrrd"
 git config --global user.email "94562196+technerrrd@users.noreply.github.com"
 sudo apt-get update -y #packages update
 sudo apt-get upgrade -y #packages upgrade
 sudo apt dist-upgrade -y
 sudo apt clean -y
+chown -R www-data:www-data /mount/sandisk
 curl -sSL https://get.docker.com 67 | sh #install docker
-sudo usermod -aG docker sniperx07 #enable docker for sniperx07
+sudo usermod -aG docker $username #enable docker for sniperx07
 cd $dir
 sudo cp -r $dir/raspberry-pi/docker/ $dir/docker
 sudo docker run hello-world 
 cd $dir/docker/combined
-sudo docker compose up -d
+sudo docker compose --env-file details.env up -d
 cd $dir/docker/postgres
 sudo docker compose up -d
 sudo rpi-update #update firmware in the end
